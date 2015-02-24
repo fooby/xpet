636893ef22d9a686bf3ba7920b29f951179dca73
Dez <dleung@lol>
2015-02-25 01:36:15 +1100

    Dog chasing physics ball

--- js/xpet/xpet.js
+++ js/xpet/xpet.js
@@ -41,6 +41,15 @@
             this.updatable = {};
 
             this.id_counter = 0;
+            this.now = timestamp();
+        },
+
+        height: function() {
+            return this.el.height();
+        },
+
+        width: function() {
+            return this.el.width();
         },
 
         loop: function() {
@@ -50,7 +59,7 @@
 
             var self = this;
             function frame() {
-                now = timestamp();
+                now = self.now = timestamp();
                 dt = dt + Math.min(1000, (now - last));
 
                 while(dt > step) {
@@ -140,7 +149,7 @@
             }).css({
                 width: this.width,
                 height: this.height
-            });
+            }).on('dragstart', function(ev) { ev.preventDefault(); });
         },
 
         calc_pos: function() {
@@ -254,10 +263,10 @@
             });
         },
 
-        go_to_pos: function(dest) {
+        go_to_pos: function(dest, dist) {
             var speed = this.SPEED;
 
-            if (this.pos.distanceSq(dest) < 5000) {
+            if (this.pos.distanceSq(dest) < (dist * dist)) {
                 this.stop();
             } else {
                 this.velocity = dest.clone().subtract(this.pos).normalize()
@@ -299,6 +308,9 @@
         init: function(options) {
             this.pos = new Victor(options.x, options.y);
             this.angle = 0;
+            this.game = options.game;
+            this.throw_time = this.game.now;
+            this.dragging = false;
 
             // Diameter
             this.diameter = options.diameter;
@@ -310,12 +322,15 @@
 
             this.physics_body = Matter.Bodies.circle(
                 this.pos.x, this.pos.y, this.diameter / 2, {
-                    restitution: 0.9,
-                    //collisionFilter: {
-                    //    category: PHYSICS_THROWABLE,
-                    //    mask: PHYSICS_WALL | PHYSICS_MOUSE
-                    //}
+                    restitution: 0.8,
+                    label: "Ball",
+                    collisionFilter: {
+                        category: PHYSICS_THROWABLE,
+                        mask: PHYSICS_WALL | PHYSICS_MOUSE
+                    }
                 });
+
+            this.physics_body.XPET = this;
         },
 
         update: function() {
@@ -327,6 +342,19 @@
             this.sprite.pos.x = this.pos.x;
             this.sprite.pos.y = this.pos.y;
             this.sprite.angle = this.angle;
+        },
+
+        physics_sleep: function() {
+            Matter.Sleeping.set(this.physics_body, true);
+        },
+
+        on_start_drag: function() {
+            this.dragging = true;
+        },
+
+        on_end_drag: function() {
+            this.dragging = false;
+            this.throw_time = this.game.now;
         }
     });
 
@@ -338,7 +366,10 @@
             this.game = options.game;
             this.setup_physics();
             this.dog = new Dog({x: 100, y: 50});
-            this.ball = new Ball({x: 200, y: 300, diameter: 25});
+            this.ball = new Ball({
+                x: 200, y: 100, diameter: 25, game: this.game});
+            Matter.Body.applyForce(this.ball.physics_body,
+                {x: 0, y: 400}, {x: 0.01, y: -0.01});
 
             this.game.register_updatable(this);
 
@@ -352,6 +383,13 @@
             }, this);
 
             Matter.World.add(this.physics_engine.world, this.ball.physics_body);
+
+            this.set_dog_mode("fetching", {target: this.ball});
+        },
+
+        set_dog_mode: function(mode, data) {
+            this.dog_mode = mode;
+            this.dog_mode_data = data;
         },
 
         setup_physics: function() {
@@ -366,21 +404,43 @@
             });
 
             // Create mouse constraint
+            var mouse = Matter.Mouse.create(this.game.el[0]);
             this.physics_mouse_constraint = Matter.MouseConstraint.create(
                 this.physics_engine, {
-                    mouse: Matter.Mouse.create(this.game.el[0]),
-                    //collisionFilter: {
-                    //    category: PHYSICS_MOUSE,
-                    //    mask: PHYSICS_THROWABLE
-                    //}
+                    mouse: mouse,
+                    collisionFilter: {
+                        category: PHYSICS_MOUSE,
+                        mask: PHYSICS_THROWABLE
+                    },
+                    constraint: Matter.Constraint.create({
+                        label: 'Mouse Constraint',
+                        pointA: mouse.position,
+                        pointB: { x: 0, y: 0 },
+                        length: 0.01,
+                        stiffness: 0.7,
+                        angularStiffness: 1
+                    })
                 });
 
+            Matter.Events.on(this.physics_mouse_constraint, 'startdrag',
+                _.bind(function(event) {
+                    var xpet_object = event.body.XPET;
+                    if (xpet_object) { xpet_object.on_start_drag(); }
+                }));
+
+
+            Matter.Events.on(this.physics_mouse_constraint, 'enddrag',
+                _.bind(function(event) {
+                    var xpet_object = event.body.XPET;
+                    if (xpet_object) { xpet_object.on_end_drag(); }
+                }));
+
             var world = this.physics_engine.world;
-            var game_width = this.game.el.width();
-            var game_height = this.game.el.height();
+            var game_width = this.game.width();
+            var game_height = this.game.height();
             
             // Extending beyond the div
-            var bounds_offset = 100;
+            var bounds_offset = 200;
 
             world.bounds.min.x = -bounds_offset;
             world.bounds.min.y = -bounds_offset;
@@ -389,27 +449,43 @@
 
             // Create walls
             var inset = 5;
-            var extra = 10;
-            var thickness = 50;
+            var extra = 50;
+            var thickness = 100;
             var half_thickness = thickness / 2;
             var half_game_width = game_width / 2;
             var half_game_height = game_height / 2;
             var r = Matter.Bodies.rectangle;
 
+            var common_options = {
+                isStatic: true,
+                collisionFilter: {
+                    category: PHYSICS_WALL,
+                    mask:  PHYSICS_THROWABLE
+                }
+            };
+
             var roof = r(half_game_width, inset-half_thickness,
-                         game_width + extra, thickness, { isStatic: true });
+                game_width + extra, thickness, _.defaults({
+                    label: "Roof"
+                }, common_options));
 
             var floor = r(half_game_width, game_height+half_thickness-inset,
-                          game_width + extra, thickness, { isStatic: true });
+                game_width + extra, thickness, _.defaults({
+                    isStatic: true,
+                    label: "Floor"
+                }, common_options));
 
             var left_wall = r(inset-half_thickness, half_game_height,
-                              game_height + extra, thickness,
-                              { isStatic: true });
+                thickness, game_height + extra, _.defaults({
+                    isStatic: true,
+                    label: "Left Wall"
+                }, common_options));
 
             var right_wall = r(game_width+half_thickness-inset,
-                               half_game_height,
-                               game_height + extra, thickness,
-                               { isStatic: true });
+                half_game_height, thickness, game_height + extra, _.defaults({
+                    isStatic: true,
+                    label: "Right Wall"
+                }, common_options));
 
             Matter.World.add(world, [
                 roof, floor, left_wall, right_wall,
@@ -431,19 +507,65 @@
             }
         },
 
-        update: function(dt) {
-            Matter.Engine.update(this.physics_engine, dt, 1);
 
-            var mouse_pos = this.get_mouse_pos();
-            if (mouse_pos) {
-                this.dog.go_to_pos(mouse_pos);
-            } else {
-                this.dog.stop();
-            }
+        update: function(dt) {
+            this.physics_update(dt);
+
+            this.game_update(dt);
 
             _.each(this.to_update, function(entity) {
                 entity.update(dt);
             }, this);
+        },
+
+        game_update: function() {
+
+            switch (this.dog_mode) {
+                case "fetching":
+                    this.dog_mode_fetching(this.dog_mode_data);
+            }
+
+        },
+
+        dog_mode_fetching: function(data) {
+            var target = data.target;
+            if (target.pos) {
+                var required_distance = 20;
+                var in_range = (target.pos.distanceSq(this.dog.pos) <
+                    (required_distance * required_distance));
+                
+                var throw_time = this.game.now - target.throw_time;
+
+                var still_throwing = throw_time < 2000;
+                console.log("Throw time: "   + throw_time);
+
+                if (target.dragging) {
+                    this.dog.stop();
+                } else if (!(still_throwing) && in_range) {
+                    this.ball.physics_sleep();
+                    this.dog.stop();
+                } else {
+                    this.dog.go_to_pos(target.pos, required_distance / 2);
+                }
+
+            } else {
+                this.dog.stop();
+            }
+        },
+
+        physics_update: function(dt) {
+            // SUPER MEGA HACK FOR MATTER MOUSE HANDLING (see
+            // _getRelativeMousePosition)
+            var el = this.game.el[0];
+            el.width = el.clientWidth;
+            el.height = el.clientHeight;
+
+            var event = {
+                timestamp: this.game.now
+            };
+            Matter.Events.trigger(this.physics_engine, 'beforeTick', event);
+            Matter.Events.trigger(this.physics_engine, 'tick', event);
+            Matter.Engine.update(this.physics_engine, dt, 1);
         }
     });
 
