8857f90b486b6edf4ec7df7fdb24e64505bab7ba
Dez <dleung@lol>
2015-02-25 00:20:15 +1100

    matter.js integration wip

--- js/xpet/xpet.js
+++ js/xpet/xpet.js
@@ -1,4 +1,4 @@
-/* global XPET, Class, Victor, Motio */
+/* global XPET, Class, Victor, Motio, Matter */
 /* jshint forin: false */
 
 // Namespace module
@@ -124,22 +124,19 @@
         "sprite-corgi-stand-right": { width: 100, height: 100, frames: 2, fps: 2 }
     };
 
-    exports.Sprite = Class.extend({
+    exports.Image = Class.extend({
         init: function(options) {
-            this.pos = options.pos;
+            this.pos = options.pos || new Victor(0, 0);
             this.width = options.width;
             this.height = options.height;
-            this.sprite = options.sprite;
-
-            this.anim = null;
-            this.motio = null;
-
+            this.angle = options.angle || 0;
+            this.css_class = options.css_class;
             this.create();
         },
 
         create: function() {
             this.el = jQuery('<div>', {
-                "class": "sprite"
+                "class": this.css_class
             }).css({
                 width: this.width,
                 height: this.height
@@ -152,12 +149,28 @@
 
         render: function() {
             var pos = this.calc_pos();
-            this.el.css({x: pos.x + 'px', y: pos.y + 'px'});
+            if (pos) {
+                var x = pos.x - this.width / 2;
+                var y = pos.y - this.height / 2;
+
+                this.el.css({
+                    x: x + 'px',
+                    y: y + 'px',
+                    rotate: this.angle + 'rad'
+                });
+            }
+        }
+    });
+
+    exports.Sprite = exports.Image.extend({
+        init: function(options) {
+            this._super(options);
+            this.anim = null;
+            this.motio = null;
         },
 
         set_animation: function(anim) {
             if (this.anim === anim) { return; }
-
             if (this.motio) { this.motio.destroy(); }
 
             this.el.removeClass();
@@ -165,21 +178,19 @@
             console.assert(anim in ANIMATION_DEFAULTS);
             var defaults = ANIMATION_DEFAULTS[anim];
             this.anim = anim;
-
             this.motio = new Motio(this.el[0], _.defaults({
                 vertical: true
             }, defaults));
-
             this.motio.play();
         }
     });
 
-    exports.MovingSprite = exports.Sprite.extend({
-        init: function(options) {
-            this._super(options);
-            this.velocity = options.velocity;
-        }
-    });
+    //exports.MovingSprite = exports.Sprite.extend({
+    //    init: function(options) {
+    //        this._super(options);
+    //        this.velocity = options.velocity;
+    //    }
+    //});
 
 })();
 
@@ -222,6 +233,10 @@
     var sprite = XPET.namespace('sprite');
     var input = XPET.namespace('input');
 
+    var PHYSICS_MOUSE = 0x002;
+    var PHYSICS_THROWABLE = 0x004;
+    var PHYSICS_WALL = 0x008;
+
     var Dog = Class.extend({
         SPEED: 300, // per second
 
@@ -234,27 +249,18 @@
             this.facing_right = true;
 
             this.sprite = new sprite.Sprite({
-                pos: this.pos,
-                velocity: this.velocity,
                 width: this.width,
-                height: this.height,
-                sprite: "dog"
+                height: this.height
             });
-        },
-
-        get_center: function() {
-            return new Victor(this.pos.x + this.width / 2,
-                this.pos.y + this.height / 2);
         },
 
         go_to_pos: function(dest) {
             var speed = this.SPEED;
-            var center = this.get_center();
 
-            if (center.distanceSq(dest) < 5000) {
+            if (this.pos.distanceSq(dest) < 5000) {
                 this.stop();
             } else {
-                this.velocity = dest.clone().subtract(center).normalize()
+                this.velocity = dest.clone().subtract(this.pos).normalize()
                     .multiply({x: speed, y: speed});
             }
         },
@@ -283,22 +289,132 @@
                 this.sprite.set_animation(this.facing_right ?
                     "sprite-corgi-stand-right": "sprite-corgi-stand-left");
             }
+
+            this.sprite.pos.x = this.pos.x;
+            this.sprite.pos.y = this.pos.y;
+        }
+    });
+
+    var Ball = Class.extend({
+        init: function(options) {
+            this.pos = new Victor(options.x, options.y);
+            this.angle = 0;
+
+            // Diameter
+            this.diameter = options.diameter;
+            this.sprite = new sprite.Image({
+                width: this.diameter,
+                height: this.diameter,
+                css_class: "ball"
+            });
+
+            this.physics_body = Matter.Bodies.circle(
+                this.pos.x, this.pos.y, this.diameter / 2, {
+                    restitution: 0.9,
+                    //collisionFilter: {
+                    //    category: PHYSICS_THROWABLE,
+                    //    mask: PHYSICS_WALL | PHYSICS_MOUSE
+                    //}
+                });
+        },
+
+        update: function() {
+            // Read position from matter
+            this.pos.x = this.physics_body.position.x;
+            this.pos.y = this.physics_body.position.y;
+            this.angle = this.physics_body.angle;
+
+            this.sprite.pos.x = this.pos.x;
+            this.sprite.pos.y = this.pos.y;
+            this.sprite.angle = this.angle;
         }
     });
 
     /**
      * This is effectively the game logic
      */
-    var DogLogic = Class.extend({
+    var DogGameLogic = Class.extend({
         init: function(options) {
             this.game = options.game;
-            this.dog = new Dog({
-                x: 100,
-                y: 50
-            });
+            this.setup_physics();
+            this.dog = new Dog({x: 100, y: 50});
+            this.ball = new Ball({x: 200, y: 300, diameter: 25});
 
             this.game.register_updatable(this);
-            this.game.register_renderable(this.dog.sprite);
+
+            // We store our own updatables
+            this.to_update = [this.dog, this.ball];
+
+            var sprites = [this.dog.sprite, this.ball.sprite];
+
+            _.each(sprites, function(s) {
+                this.game.register_renderable(s);
+            }, this);
+
+            Matter.World.add(this.physics_engine.world, this.ball.physics_body);
+        },
+
+        setup_physics: function() {
+            this.physics_engine = Matter.Engine.create(this.game.el[0], {
+                render: {
+                    controller: {
+                        create: function() {},
+                        clear: function() {},
+                        world: function() {}
+                    }
+                }
+            });
+
+            // Create mouse constraint
+            this.physics_mouse_constraint = Matter.MouseConstraint.create(
+                this.physics_engine, {
+                    mouse: Matter.Mouse.create(this.game.el[0]),
+                    //collisionFilter: {
+                    //    category: PHYSICS_MOUSE,
+                    //    mask: PHYSICS_THROWABLE
+                    //}
+                });
+
+            var world = this.physics_engine.world;
+            var game_width = this.game.el.width();
+            var game_height = this.game.el.height();
+            
+            // Extending beyond the div
+            var bounds_offset = 100;
+
+            world.bounds.min.x = -bounds_offset;
+            world.bounds.min.y = -bounds_offset;
+            world.bounds.max.x = game_width + bounds_offset;
+            world.bounds.max.y = game_height + bounds_offset;
+
+            // Create walls
+            var inset = 5;
+            var extra = 10;
+            var thickness = 50;
+            var half_thickness = thickness / 2;
+            var half_game_width = game_width / 2;
+            var half_game_height = game_height / 2;
+            var r = Matter.Bodies.rectangle;
+
+            var roof = r(half_game_width, inset-half_thickness,
+                         game_width + extra, thickness, { isStatic: true });
+
+            var floor = r(half_game_width, game_height+half_thickness-inset,
+                          game_width + extra, thickness, { isStatic: true });
+
+            var left_wall = r(inset-half_thickness, half_game_height,
+                              game_height + extra, thickness,
+                              { isStatic: true });
+
+            var right_wall = r(game_width+half_thickness-inset,
+                               half_game_height,
+                               game_height + extra, thickness,
+                               { isStatic: true });
+
+            Matter.World.add(world, [
+                roof, floor, left_wall, right_wall,
+                this.physics_mouse_constraint
+            ]);
         },
 
         get_mouse_pos: function() {
@@ -316,18 +432,22 @@
         },
 
         update: function(dt) {
-            var mouse_pos = this.get_mouse_pos();
+            Matter.Engine.update(this.physics_engine, dt, 1);
 
+            var mouse_pos = this.get_mouse_pos();
             if (mouse_pos) {
                 this.dog.go_to_pos(mouse_pos);
             } else {
                 this.dog.stop();
             }
-            this.dog.update(dt);
+
+            _.each(this.to_update, function(entity) {
+                entity.update(dt);
+            }, this);
         }
     });
 
     exports.setup_dog = function(game) {
-        var dog = new DogLogic({game: game});
+        var dog = new DogGameLogic({game: game});
     };
 })();
